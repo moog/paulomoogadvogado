294ba75b9dfa0c9439b253575e290745182e0098
MateusMoog <mateus.moog@gmail.com>
2016-03-28 11:44:47 -0300

    Ajustes na responsividade

---
+++ js/contato.js
@@ -0,0 +1,20 @@
+var Contato = function {
+  var priv = [];
+  var pub = [];
+
+  priv.isValid = function() {
+    
+  }
+
+  priv.validarForumlario = function(){
+
+    var nome = $("#formNome").val();
+    var email = $("#formEmail").val();
+    var telefone = $("#formTelefone").val();
+    var mensagem = $("formMensagem").val();
+
+    if(nome.length <= 2)
+
+  };
+
+}();
